e3c4616d84da26e032b96a3c5d7e8594b0fce94c
Monika Kumar <mona.016@gmail.com>
2012-10-08 09:53:31 -0700

    Homework 1

---
+++ Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am ready to learn and ask questions.");
+            Console.ReadLine();
+        }
+        
+    }
+}
